42488b7466e7d3d080c376372df391f931581216
akshayasenthil28 <akshayasenthil00@gmail.com>
2026-07-13 20:10:24 +0530

    change origins

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -30,6 +30,8 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
                 "origins": [
                     "http://localhost:5173",
                     "http://127.0.0.1:5173",
+                    "http://localhost:5174",
+                    "http://127.0.0.1:5174",
                 ]
             }
         },
